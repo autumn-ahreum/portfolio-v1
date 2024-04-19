ded5d1c3a38cfda66b4b589afc0c948e6d395cf3
autumn-ahreum <hanjiro77@gmail.com>
2024-04-19 12:07:44 -0700

    Set up Tailwind and Routers

---
+++ src/routers/AppRouter.jsx
@@ -0,0 +1,28 @@
+import { BrowserRouter, Routes, Route, RouterProvider } from "react-router-dom"
+
+// components 
+import Header from "../components/Header";
+import Footer from "../components/Footer";
+
+// pages
+import HomePage from "../pages/HomePage";
+import AboutPage from "../pages/AboutPage";
+import WorksPage from "../pages/WorksPage";
+import NotFoundPage from "../pages/NotFoundPage";
+
+const AppRouter = () => {
+  return ( 
+    <BrowserRouter>
+        <Header />
+        <Routes>
+            <Route path="/" element={<HomePage />}/>
+            <Route path="/about" element={<AboutPage />}/>
+            <Route path="/works" element={<WorksPage />}/>
+            <Route path="*" element={<NotFoundPage />}/>
+        </Routes>
+        <Footer />
+    </BrowserRouter>
+  )
+}
+
+export default AppRouter
