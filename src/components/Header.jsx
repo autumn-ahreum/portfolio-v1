f361789074099bfdfdc4e3db9df6b99936b9a6ba
autumn-ahreum <hanjiro77@gmail.com>
2024-08-31 17:47:05 -0700

    add header aria info

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -11,12 +11,19 @@ const Header = () => {
   const toggleMenu = () => {
     setOpened(!opened);
   };
+
+  const handleKeyDown = (e) => {
+    if (e.key === 'Enter' || e.key === ' ') {
+      toggleMenu();
+    }
+  };
+  
  
   return (
     <header id='header' className={classNames(`pt-1.5 max-w-[1920px] mx-auto`, { 'show': opened })}>
       <div className="first-low-container flex justify-between w-full">
         <div className="logo-container ml-1">
-          <Link to="/"><img src="/images/logo.png" alt="Logo" className= "max-w-32"  /></Link>
+          <Link to="/"><img src="/images/logo.png" alt="Move to Home" className= "max-w-32"  /></Link>
         </div>
         {/** 
         * found at this url : 
@@ -25,7 +32,13 @@ const Header = () => {
         <div className={classNames(`tham tham-e-squeeze tham-w-5 mt-6 ml-2 mr-3 cursor-pointer`, { 'tham-active': opened}) } 
           onMouseDown= {(e) => { e.preventDefault(); }}
           onClick={toggleMenu}
-          id="header">
+          onKeyDown={handleKeyDown}
+          id="header"
+          aria-label="Open Menu"
+          aria-expanded={opened}
+          role="button"
+          tabIndex={0} // to receive a keyboard focusing
+        >
           <div className="tham-box">
             <div className={classNames(`tham-inner bg-p-color`, 
             { 'bg-s-color': opened, 'z-40': opened}) }/>
